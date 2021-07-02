29051a78bc63b7367b1c102a043764c4813f022f
Tim Garrity <tag.garrity@gmail.com>
2021-07-02 11:18:56 -0400

    different log strategies, logs fatal panic and fmt have a time and place

---
+++ errorHandling/179/err_logging.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	_, err := os.Open("no-file.txt")
+	if err != nil {
+		// fmt provides basic text output no frills
+		// fmt.Println("err happened", err)
+		// log prints date timestamp
+		// log.Println("err happened", err)
+		// fatal calls os.Exit(1) when called
+		log.Fatal(err)
+	}
+
+}

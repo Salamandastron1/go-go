51c5916f9c5713f9e061ce6e33234a3603aecd9d
Tim Garrity <tag.garrity@gmail.com>
2021-07-08 13:00:28 -0400

    package dog and go-go rename to gogo

---
+++ documentation/ninja_level_12/dog/dog.go
@@ -0,0 +1,7 @@
+// Package dog does all the dogone work
+package dog
+
+// Years turns humans years into dogs years
+func Years(y int) int {
+	return y * 7
+}
---
+++ documentation/ninja_level_12/main.go
@@ -0,0 +1,8 @@
+package main
+import (
+	"github.com/Salamandastron1/go-go/documentation/ninja_level_12/dog"
+)
+func main()  {
+	
+}
+

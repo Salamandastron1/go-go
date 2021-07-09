073fba9d7a133d25f0eb8b9ab1c08063152c4fb2
Tim Garrity <tag.garrity@gmail.com>
2021-07-09 12:25:51 -0400

    table tests allow for multiple test cases at once

---
+++ testing_and_benchmarking/194_table_tests/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestMySum(t *testing.T) {
+	type test struct {
+		data     []int
+		expected int
+	}
+	cases := []test{
+		{[]int{1, 2, 3, 4, 5}, 15},
+		{[]int{5, 5, 5}, 15	},
+		{[]int{1, 1, 1, 1, 1}, 5},
+	}
+
+	for _, v := range cases {
+		actual := mySum(v.data...)
+		if v.expected != actual {
+			t.Errorf("Expected: %v Got: %v", v.expected, actual)
+		}
+	}
+}
